16437624f2f0bf196f2d170286470f8aafcd9a9e
gisele <gisesonia@gmail.com>
2024-07-04 17:35:44 -0300

    criando a viewcontroller de detalhes de receitas

--- ReceitasApp/DetailsRecipeViewController.swift
+++ ReceitasApp/DetailsRecipeViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class DetailsRecipeViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var ingredientsTextView: UITextView!
+    @IBOutlet weak var favoriteButton: UIButton!
+  
+    @IBOutlet weak var howTo: UITextView!
 
+    
+    var recipe: Recipe?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Verifica se há uma receita atribuída
+        guard let recipe = recipe else {
+            return
+        }
+        
+        // Configura os elementos da UI com os dados da receita
+        imageView.image = UIImage(named: recipe.image)
+        nameLabel.text = recipe.name
+        authorLabel.text = recipe.author
+        ingredientsTextView.text = recipe.ingredients.joined(separator: "\n")
+        howTo.text = recipe.howTo.joined(separator: "\n")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Exemplo de ação para o botão de favoritos
+    @IBAction func favoritarButtonTapped(_ sender: UIButton) {
+        // Implemente a lógica para favoritar a receita
+        // Você pode usar a estrutura receita e a funcionalidade de favoritos existente
     }
-    */
-
 }
+
