ec66939208988b0ef12d11e3d4b115dbf5805e72
gisele <gisesonia@gmail.com>
2024-07-04 09:03:43 -0300

    Initial Commit

---
+++ ReceitasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReceitasApp
+//
+//  Created by GISELE TOLEDO on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
