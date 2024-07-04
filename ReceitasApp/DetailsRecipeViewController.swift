8917261f717f821fe30fab8db1f785a30270539a
gisele <gisesonia@gmail.com>
2024-07-04 17:32:18 -0300

    tela de detalhes da receita

---
+++ ReceitasApp/DetailsRecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsRecipeViewController.swift
+//  ReceitasApp
+//
+//  Created by GISELE TOLEDO on 04/07/24.
+//
+
+import UIKit
+
+class DetailsRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
