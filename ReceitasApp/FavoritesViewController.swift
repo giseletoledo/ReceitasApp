5a39f9f3f68f7b585e1b90e7aec847d0e1ca5c30
gisele <gisesonia@gmail.com>
2024-07-07 12:12:15 -0300

    Adicionando uisearchbar e title ao layout

--- ReceitasApp/FavoritesViewController.swift
+++ ReceitasApp/FavoritesViewController.swift
@@ -9,49 +9,61 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var titleLabel: UILabel!
     
     var favoriteRecipes: [Recipe] = []
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
-        loadFavorites()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            favoriteRecipes = recipes
+            tableView.dataSource = self
+            tableView.delegate = self
+            loadFavorites()
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            loadFavorites()
+        }
+        
+        func loadFavorites() {
+            favoriteRecipes = recipes.filter { UserDefaults.standard.bool(forKey: "FavoriteRecipe-\( $0.name)") }
+            tableView.reloadData()
+        }
+}
+
+    extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return favoriteRecipes.isEmpty ? 1 : favoriteRecipes.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            if favoriteRecipes.isEmpty {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "EmptyCell", for: indexPath)
+                cell.textLabel?.text = "Nenhuma receita favorita encontrada."
+                return cell
+            } else {
+                let recipe = favoriteRecipes[indexPath.row]
+                let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath)
+                cell.textLabel?.text = recipe.name
+                cell.textLabel?.lineBreakMode = .byWordWrapping
+                cell.detailTextLabel?.text = recipe.author
+                cell.imageView?.image = recipe.image != nil ? UIImage(named: recipe.image!) : UIImage(named: "defaultImage")
+                
+                return cell
+            }
+        }
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        loadFavorites()
-    }
-    
-    func loadFavorites() {
-        favoriteRecipes = recipes.filter { UserDefaults.standard.bool(forKey: "FavoriteRecipe-\( $0.name)") }
+
+extension FavoritesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            favoriteRecipes = recipes
+        } else {
+            favoriteRecipes = favoriteRecipes.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        }
         tableView.reloadData()
     }
 }
 
-extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Se não houver receitas favoritas, mostrar uma célula com mensagem
-        return favoriteRecipes.isEmpty ? 1 : favoriteRecipes.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Se não houver receitas favoritas, mostrar a célula especial com mensagem
-        if favoriteRecipes.isEmpty {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EmptyCell", for: indexPath)
-            cell.textLabel?.text = "Nenhuma receita favorita encontrada."
-            cell.detailTextLabel?.text = ""
-            cell.imageView?.image = nil
-            return cell
-        } else {
-            // Caso contrário, configurar a célula normalmente com as receitas favoritas
-            let recipe = favoriteRecipes[indexPath.row]
-            let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath)
-            cell.textLabel?.text = recipe.name
-            cell.detailTextLabel?.text = recipe.author
-            cell.imageView?.image = recipe.image != nil ? UIImage(named: recipe.image!) : UIImage(named: "placeholder")
-            return cell
-        }
-    }
-}
