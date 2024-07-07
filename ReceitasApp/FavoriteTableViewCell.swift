654884e9e3c4ca0340e1df1415b32b1f3bb97ba9
gisele <gisesonia@gmail.com>
2024-07-07 12:11:11 -0300

    Adicionando icone ao layout de heart

--- ReceitasApp/FavoriteTableViewCell.swift
+++ ReceitasApp/FavoriteTableViewCell.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class FavoriteTableViewCell: UITableViewCell {
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+
+    var isFavorite: Bool = false {
+        didSet {
+            let heartImage = isFavorite ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
+            favoriteButton.setImage(heartImage, for: .normal)
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
