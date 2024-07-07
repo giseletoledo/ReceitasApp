12eaa6fb62ccb3c9df906c11276f81c95fc14007
gisele <gisesonia@gmail.com>
2024-07-07 12:08:28 -0300

    Ajustando para visualizar por categorias e link para tela de detalhes de receita

--- ReceitasApp/CategoriesViewController.swift
+++ ReceitasApp/CategoriesViewController.swift
@@ -8,14 +8,8 @@
 import UIKit
 
 class CategoriesViewController: UIViewController {
-
-    @IBOutlet weak var collectionView: UICollectionView!
     
-    var categories: [Category] = [
-        Category(name: "Dessert", image: "bolo.jpg"),
-        Category(name: "Snack", image: "coxinha.jpg"),
-        Category(name: "Main Course", image: "hamburguer.jpg")
-    ]
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +34,23 @@ extension CategoriesViewController: UICollectionViewDataSource, UICollectionView
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as! CategoryCollectionViewCell
         
         let category = categories[indexPath.row]
-        cell.imageView.image = UIImage(named: category.image ?? "defaultImage")
-        cell.categoryNameLabel.text = category.name
+        cell.imageView.image = UIImage(named: category.imageName)
+        cell.categoryNameLabel.text = category.rawValue
         cell.layer.cornerRadius = 8
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let category = categories[indexPath.item]
+        
+        // Navigate to CategoriesListViewController
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let categoriesListVC = storyboard.instantiateViewController(withIdentifier: "CategoriesListViewController") as? CategoriesListViewController {
+            categoriesListVC.recipes = recipes.filter { $0.category == category }
+            categoriesListVC.title = category.rawValue
+            categoriesListVC.navigationItem.backButtonTitle = "Categories" // Custom back button title
+            navigationController?.pushViewController(categoriesListVC, animated: true)
+        }
     }
 }
